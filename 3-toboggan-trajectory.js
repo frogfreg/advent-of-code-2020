40eafdde2fc2a70fcaba2a7d09630f7199a28c1d
Guillermo Fernandez <bengallme@gmail.com>
2020-12-03 10:31:09 -0600

    added solution to day 3 part 1

--- 3-toboggan-trajectory.js
+++ 3-toboggan-trajectory.js
@@ -1,3 +1,21 @@
 const inputArr = require("./input3.json");
 
-console.log(inputArr);
+const longInputArr = inputArr.map((row) => {
+  return row.repeat(34);
+});
+
+function countTrees(input) {
+  let counter = 0;
+  let i = 0;
+  let j = 0;
+  while (input[i]) {
+    if (input[i][j] === "#") {
+      counter++;
+    }
+    i += 1;
+    j += 3;
+  }
+  return counter;
+}
+
+console.log(countTrees(longInputArr));
