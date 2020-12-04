574f4e2f4e2ff7f3279e8ed7405d723801cbbe78
Guillermo Fernandez <bengallme@gmail.com>
2020-12-04 15:14:23 -0600

    Added solution for day 4 part 1

--- 4-1-passport-proccesing.js
+++ 4-1-passport-proccesing.js
@@ -1,17 +1,20 @@
 const inputArr = require("./input4.json");
 
-const passport = inputArr[0].split(" ");
+const validatePassport = (string) => {
+  const checkValues = ["byr:", "iyr:", "eyr:", "hgt:", "hcl:", "ecl:", "pid:"];
+  for (let value of checkValues) {
+    if (!string.includes(value)) {
+      return false;
+    }
+  }
+  return true;
+};
 
-const key = passport[0].split(":");
-
-const [keyName, value] = key;
-
-const obj = {};
-obj[keyName] = value;
-
-console.log(obj);
-
-function objectize(string) {
-  const keysArr = string.split(" ");
-    
-}
+const result = inputArr.reduce((acc, cur) => {
+  if (validatePassport(cur)) {
+    return acc + 1;
+  } else {
+    return acc;
+  }
+}, 0);
+console.log(result);
